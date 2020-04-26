caf22bd1b7a420260f2b8c887341d8a40af6c578
Jacob Young <jyoung17@uoguelph.ca>
2020-04-26 16:57:34 -0400

    Updated encryption to read key and message from a file

--- src/Encryption.java
+++ src/Encryption.java
@@ -4,26 +4,30 @@ public class Encryption {
 
 	public static void main(String[] args) throws IOException {
 	    if (args.length != 2) {
-	    	System.err.println("The program needs two files to execute");
+	    	System.err.println("The program needs two txt files to execute");
+	    	System.out.println("The files are ordered: key.txt message.txt");
 	    	System.exit(1);
 	    }
 		final String secretKey = setKey(args[0]);
 	    
-	    /*
-	    FileWriter fw = new FileWriter(args[0].replace("txt", "pwde"));
-	    PrintWriter pw = new PrintWriter(fw);
-	    
-	    pw.close();
-	    fw.close();
-	    */
-	    
-	    String originalString = "The code word is pineapple";
-	    String encryptedString = AES.encrypt(originalString, secretKey);
-	    String decryptedString = AES.decrypt(encryptedString, secretKey);
-	    
-	    System.out.println(originalString);
-	    System.out.println(encryptedString);
-	    System.out.println(decryptedString);
+		File secretFile = new File(args[1]);
+		FileReader fr = new FileReader(secretFile);
+		BufferedReader br = new BufferedReader(fr);
+		
+		FileWriter fw = new FileWriter(args[1].replace("txt", "encrypt"));
+		PrintWriter pw = new PrintWriter(fw);
+		
+		String line;
+		while((line = br.readLine()) != null) {
+			String encryptedString = AES.encrypt(line, secretKey);
+			System.out.println(line);
+			System.out.println(encryptedString);
+			pw.printf("%s\n", encryptedString);
+			//String decryptedString = AES.decrypt(encryptedString, secretKey);
+			//System.out.println(decryptedString);
+		}
+		
+		closeFiles(br, fr, pw, fw);
 	}
 	
 	public static String setKey(String fileName) throws IOException {
@@ -37,6 +41,13 @@ public class Encryption {
 		fr.close();
 		
 		return key;
+	}
+	
+	public static void closeFiles(BufferedReader br, FileReader fr, PrintWriter pw, FileWriter fw) throws IOException {
+		br.close();
+		fr.close();
+		pw.close();
+		fw.close();
 	}
 
 }
